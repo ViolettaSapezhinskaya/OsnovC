befeb62b1e11e4979b6b5c5abf293688b2488581
Violetta Sapezhinskaya <Arinasap10@gmail.com>
2021-12-14 12:36:58 +0300

    решения

---
+++ Ex1/Program.cs
@@ -0,0 +1,20 @@
+﻿//Написать метод, возвращающий минимальное из трёх чисел.
+void Met(int x, int y, int z)
+{
+    int min = x;
+    if (min > y)
+    {
+        min = y;
+    }
+    if (min>z)
+    {
+        min=z;
+    }
+    Console.WriteLine($"Минимальное число {min}");
+    
+}
+int a=Convert.ToInt32(Console.ReadLine()); 
+int b=Convert.ToInt32(Console.ReadLine());
+int c=Convert.ToInt32(Console.ReadLine());
+Met(a,b,c);
+
