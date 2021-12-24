fc643ab1024ff3155a71346c2f8699e42aeeb697
Violetta Sapezhinskaya <Arinasap10@gmail.com>
2021-12-24 12:54:58 +0300

    решения

---
+++ Ex01/Array.cs
@@ -0,0 +1,48 @@
+﻿class Array
+{
+    public int[] a;
+    //заполнение массива рандомными значениями
+    public Array(int n, int min, int max)
+    {
+        a = new int[n];
+        Random rnd = new Random();
+        for (int i = 0; i < n; i++)
+            a[i] = rnd.Next(min, max);
+
+    }
+    //вывод массива
+    public override string ToString()
+    {
+        string s = "";
+        for (int i = 0; i < a.Length; i++)
+        {
+            s += a[i].ToString();
+            s += ";";
+        }
+        return s;
+    }
+    // вывести количество пар элементов массива, в которых только одно число делится на 3
+    public int Pair()
+    {
+        int x = 0;
+        for (int i = 0; i < a.Length - 1; i++)
+        {
+            if (a[i] % 3 == 0)
+            {
+                if (a[i + 1] % 3 != 0)
+                    //{
+                    x++;
+            }
+    
+            else
+        {
+            if (a[i + 1] % 3 == 0)
+            {
+                x++;
+            }
+        }
+    }
+        return x;
+    }
+
+  }
---
+++ Ex2/Array.cs
@@ -0,0 +1,37 @@
+﻿class Array
+{
+    static public int Pair(int[] a)
+    {
+        int x = 0;
+        for (int i = 0; i < a.Length - 1; i++)
+        {
+            if (a[i] % 3 == 0)
+            {
+                if (a[i + 1] % 3 != 0)
+                    //{
+                    x++;
+            }
+
+            else
+            {
+                if (a[i + 1] % 3 == 0)
+                {
+                    x++;
+                }
+            }
+        }
+        return x;
+    }
+    static public int[] MyArray(string path)
+    {
+        StreamReader sr = new StreamReader(path);
+        int n = int.Parse(sr.ReadLine());
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            int b = int.Parse(sr.ReadLine());
+            a[i] = b;
+        }
+        return a;
+    }
+}
