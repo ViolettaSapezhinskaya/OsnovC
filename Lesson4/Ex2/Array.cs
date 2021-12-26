82b6a9a1538a2e13d8149c48806c774951b58a14
Violetta Sapezhinskaya <Arinasap10@gmail.com>
2021-12-26 18:58:58 +0300

    j

---
+++ Lesson4/Ex2/Array.cs
@@ -0,0 +1,45 @@
+﻿class Array
+{
+    static public int Pair(int[] a)
+    {
+        int x = 0;
+        for (int i = 0; i < a.Length - 1; i++)
+        {
+            if (a[i] % 3 == 0)
+            {
+                if (a[i + 1] % 3 != 0)
+                    //{
+                    x++;
+            }
+
+            else
+            {
+                if (a[i + 1] % 3 == 0)
+                {
+                    x++;
+                }
+            }
+        }
+        return x;
+    }
+    static public int[] MyArray(string path)
+    {
+        int[] a=new int[1];
+        try
+        {
+            StreamReader sr = new StreamReader(path);
+            int n = int.Parse(sr.ReadLine());
+            a = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                int b = int.Parse(sr.ReadLine());
+                a[i] = b;
+            }
+        }
+        catch
+        {
+            Console.WriteLine("Что-то пошло не так");
+        }
+        return a;
+    }
+}
---
+++ Lesson4/Ex2/Program.cs
@@ -0,0 +1,12 @@
+﻿//Реализуйте задачу 1 в виде статического класса StaticClass;
+//а) Класс должен содержать статический метод, который принимает на вход массив и решает задачу 1;
+int[] a = new int[5] { 6, 2, 9, 3, 6 };
+int y = Array.Pair(a);
+Console.WriteLine(y);
+//б) *Добавьте статический метод для считывания массива из текстового файла. Метод должен возвращать массив целых чисел;
+string text1 = "C:\\Users\\User\\source\\repos\\OsnovC2\\Ex2\\TextFile01.txt";
+int[] b = Array.MyArray(text1);
+int x = Array.Pair(b);
+//в)**Добавьте обработку ситуации отсутствия файла на диске
+string text2 = "C:\\Users\\User\\source\\repos\\OsnovC2\\Ex2\\TextFile1.txt";
+int[] c = Array.MyArray(text1);
