32d9863042e1d444f4661fe715ff01faa52d66d5
Violetta Sapezhinskaya <Arinasap10@gmail.com>
2021-12-27 20:13:54 +0300

    решения

---
+++ Ex1/Program.cs
@@ -0,0 +1,47 @@
+﻿//оздать программу, которая будет проверять корректность ввода логина. Корректным логином будет строка от 2 до 10 символов, 
+//содержащая только буквы латинского алфавита или цифры, при этом цифра не может быть первой:
+//а) без использования регулярных выражений;
+//б) **с использованием регулярных выражений.
+using System.Text.RegularExpressions;
+using System.Text;
+
+
+
+//проверка логина без использования регулярных выражений
+StringBuilder CheckLogin(StringBuilder login)
+{
+    StringBuilder text = new();
+    bool flag = true;
+    if (login.Length > 10 | login.Length < 2)
+    {
+        text.Append("Ваш логин неверной длины.");
+        flag = false;
+    }
+    if (Char.IsDigit(login[0]))
+    {
+        text.Append("Логин не может начинаться с цифры.");
+        flag = false;
+    }
+    if (true)
+    {
+        for (int i = 0; i < login.Length; i++)
+        {
+            if (!(Char.IsDigit(login[i]) || login[i] >= 'a' && login[i] <= 'z' || login[i] >= 'A' && login[i] <= 'Z'))
+            {
+                text.Append("В вашем логине есть некоректные символы.");
+                flag = false;
+                break;
+            }
+        }
+    }
+    if (flag)
+    {
+        text.Append("Ваш логин коректен");
+    }
+    return text;
+}
+
+StringBuilder login = new(Console.ReadLine());
+StringBuilder text;
+text= CheckLogin(login);
+Console.WriteLine(text);
---
+++ Ex2/Message.cs
@@ -0,0 +1,82 @@
+﻿class Message
+{
+    //Вывести только те слова сообщения, которые содержат не более n букв.
+    static public string[] wordsN(string text, int n)
+    {
+        string[] t = text.Split(" ");
+        int k = 0;
+        for (int i = 0; i < t.Length; i++)
+        {
+            if (t[i].Length == n)
+            { k++; }
+        }
+        string[] words = new string[k];
+        k = 0;
+        for (int i = 0; i < t.Length; i++)
+        {
+            if (t[i].Length == n)
+            { 
+                words[k++] = t[i];
+            }
+        }
+        return words;
+    }
+    //вывод массива
+    static public string Print(string[] a)
+    {
+        string s = "";
+        for (int i = 0; i < a.Length; i++)
+        {
+            s += a[i].ToString();
+            s += " ";
+        }
+        return s;
+    }
+    //Удалить из сообщения все слова, которые заканчиваются на заданный символ
+    static public string removal(string text, char n)
+    {
+        string[] t = text.Split(" ");
+        string[] s = new string[t.Length];
+        for (int i = 0;i < t.Length; i++)
+        {
+            char[] c = t[i].ToCharArray();
+            if (char.IsPunctuation(c[c.Length - 1]))
+            {
+                if (c.Length > 2)
+                {
+                    if (c[c.Length - 2] != n)
+                    {
+                        s[i] = t[i] + ' ';
+                    }
+                }
+
+            }
+            else
+            {
+                if (c[c.Length - 1] != n)
+                {
+                    s[i] = t[i] + ' ';
+                }
+            }
+        }
+        string text2 = String.Concat(s);
+        return text2;
+    }
+    //Найти самое длинное слово сообщения
+    static public string wordmax(string text)
+    {
+        string word=" ";
+        string[] t = text.Split(" ");
+        int max = 0;
+        for (int i = 0; i < t.Length; i++)
+        {
+            if (t[i].Length>max)
+            {
+                max = t[i].Length;
+                word= t[i];
+            }
+        }
+        return word;
+    }
+
+}
